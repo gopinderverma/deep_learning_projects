90cebe316e09cb9d0eba1516edeabf47c692e660
gopinderverma <gopinder.verma@gmail.com>
2017-05-17 16:06:02 +0530

    violations

--- deep_learning/deep_learning/app_run.bat
+++ deep_learning/deep_learning/app_run.bat
@@ -3,4 +3,5 @@ call env/Scripts/activate.bat
 pip install requirements
 mkdir results
 call pytest --junitxml=results/unit_test_err.xml deep_learning/deep_learning/test_mnist.py
+pep8 deep_learning/deep_learning/test_mnist.py >> results/violations.log
 call env/Scripts/deactivate
