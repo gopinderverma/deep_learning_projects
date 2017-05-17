c71d146f72cd8d5afbc5d0bc29ae6d1f72595ee5
gopinderverma <gopinder.verma@gmail.com>
2017-05-17 14:48:59 +0530

    xml added

--- deep_learning/deep_learning/app_run.bat
+++ deep_learning/deep_learning/app_run.bat
@@ -2,5 +2,5 @@ virtualenv env
 call env/Scripts/activate.bat
 pip install requirements
 mkdir results
-call pytest --junitxml=results/unit_test_err.log deep_learning/deep_learning/test_mnist.py
+call pytest --junitxml=results/unit_test_err.xml deep_learning/deep_learning/test_mnist.py
 call env/Scripts/deactivate
